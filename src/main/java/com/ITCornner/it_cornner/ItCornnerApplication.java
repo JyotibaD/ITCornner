02da0ceba4eb86bae63eddfbab2ec662e89e794e
JyotibaD <jyotiba.dabade@outlook.com>
2024-07-31 17:20:48 +0530

    this is a frist commit of ITCornner BlogApplication

---
+++ src/main/java/com/ITCornner/it_cornner/ItCornnerApplication.java
@@ -0,0 +1,13 @@
+package com.ITCornner.it_cornner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItCornnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItCornnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ITCornner/it_cornner/ItCornnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITCornner.it_cornner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItCornnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
